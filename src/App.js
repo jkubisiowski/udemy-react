231e099ddf9c1e61aa3eaeeefe6609fec82bed35
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2018-10-12 20:27:12 +0200

    Add state to person component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [{
+      name: 'Max', age: 28
+    }, {
+      name: 'Stephanie', age: 33
+    }]
+  };
+  
+  switchNameHandler = () => {    
+      this.setState({persons: [{
+              name: 'Kuba', age: 28
+          }, {
+              name: 'Stephanie', age: 33
+          }]})
+  };
+  
   render() {
     return (
       <div className="App">
         <h1>Hi</h1>
-      </div>
+          <button onClick={this.switchNameHandler}>Switch name</button>
+          <Person name={this.state.persons[0].name}/>
+          <Person name={this.state.persons[1].name}/>
+      </div>      
     );
   }
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const person = (props) => {
+  return (
+      <div>
+          <p>I'm a {props.name}</p>  
+          <p>{props.children}</p>
+      </div>      
+  ) 
+    
+};
+
+export default person;
