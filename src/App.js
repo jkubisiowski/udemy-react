b5ac80511c086dff339ecea7237e3c75d0721b77
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2018-10-24 09:23:19 +0200

    Implement updating elements in list after input changed

--- src/App.js
+++ src/App.js
@@ -1,48 +1,94 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
   state = {
-    persons: [{
-      name: 'Max', age: 28
-    }, {
-      name: 'Stephanie', age: 33
-    }]
+    persons: [
+      {
+        id: 1,
+        name: "Max",
+        age: 28
+      },
+      {
+        id: 2,
+        name: "Stephanie",
+        age: 33
+      },
+      {
+        id: 3,
+        name: "Lolek",
+        age: 21
+      }
+    ],
+    showPersons: true
   };
-  
-  switchNameHandler = (newName) => {    
-      this.setState({persons: [{
-              name: newName, age: 28
-          }, {
-              name: 'Stephanie', age: 33
-          }]})
+
+  nameChangedHandler = (event, key) => {
+    let personIndex = this.state.persons.findIndex(x=>x.id===key);
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({
+      persons: persons
+    })
   };
-  
-  nameChangedHandler = event => {
-      this.setState({persons: [{
-              name: event.target.value, age: 28
-          }, {
-              name: 'Stephanie', age: 33
-          }]})
+
+  togglePersonHandler = () => {
+    this.setState({
+      showPersons: !this.state.showPersons
+    });
   };
-  
+
+  deletePersonHandler = index => {
+    const persons = this.state.persons.slice();
+    persons.splice(index, 1);
+    this.setState({
+      persons: persons
+    });
+  };
+
   render() {
-      const style = {
-          backgroundColor: 'white',
-          font: 'inherit',
-          border: '1px solid blue',
-          padding: '8px',
-          cursor: 'pointer'
-      };
-      
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+                click={() => this.deletePersonHandler(index)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi</h1>
-          <button style={style} onClick={this.switchNameHandler.bind(this, 'My new name')}>Switch name</button>
-          <Person  name={this.state.persons[0].name} click={()=>this.switchNameHandler('Lolek')}/>
-          <Person name={this.state.persons[1].name} click={this.switchNameHandler.bind(this, '2')} changed={this.nameChangedHandler}/>
-      </div>      
+        <button style={style} onClick={this.togglePersonHandler}>
+          Toggle check
+        </button>
+        {persons}
+      </div>
     );
   }
 }
